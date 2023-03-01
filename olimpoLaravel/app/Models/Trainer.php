72f295fca1c775957054e0e889cee6e9a08864e8
sebas <manuel_sebastiancriollo@elcampico.org>
2023-03-01 12:27:39 +0100

    añadiendo funcionalidad de enviar emails para recuperar la contraseña

--- olimpoLaravel/app/Models/Trainer.php
+++ olimpoLaravel/app/Models/Trainer.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
 class Trainer extends Authenticatable
 {
